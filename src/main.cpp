f6fa2e3913f177ce2c15fddfb771b8852d68bed8
Khang Vu <khangv@sfu.ca>
2020-02-07 23:15:07 -0800

    Implement horizontal bat and bat movements

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include "../headers/bat_class.h"
+#include <SFML/Graphics.hpp>
+#include <sstream>
+#include <cstdlib>
+
+using namespace sf;
+
+int windowWidth;
+int windowHeight;
+
+int main() 
+{
+	windowWidth = 1024;
+	windowHeight = 768;
+
+	Bat bat(windowWidth / 2, windowHeight - 20);
+
+	RenderWindow window(VideoMode(windowWidth, windowHeight), "Pong");
+
+    while (window.isOpen())
+    {
+        /*
+            Handle the player input
+            *********************************************************************
+            *********************************************************************
+            *********************************************************************
+        */
+
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                // Someone closed the window- bye
+                window.close();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            // move left...
+            bat.moveLeft();
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            // move right...
+            bat.moveRight();
+        }
+        else if (Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            // quit...
+            // Someone closed the window- bye
+            window.close();
+        }
+        /*
+   Draw the frame
+   *********************************************************************
+   *********************************************************************
+   *********************************************************************
+*/
+        bat.update();
+// Clear everything from the last frame
+        window.clear(Color(26, 128, 182, 255));
+
+        window.draw(bat.getShape());
+
+
+
+        // Show everything we just drew
+        window.display();
+    }// This is the end of the "while" loop
+
+    return 0;
+    }
